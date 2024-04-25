377e798a48129e2ff5e9d98e6cb173a981e5e7f6
Kevin <60121349+khebinhh@users.noreply.github.com>
2024-04-25 06:03:27 +0000

    finished parse function

--- project2/main.py
+++ project2/main.py
@@ -16,6 +16,25 @@ def parse(input_string):
     input_string += '$'
     index = 0
 
+    while len(stack) > 0:
+      top = stack.pop()
+      current_symbol = input_string[index]
+  
+      print(f"Stack: {stack}")
+  
+      if top == current_symbol:
+        index += 1
+      elif top in grammar:
+        production = parsing_table.get((top, current_symbol))
+        if production:
+          stack.extend(reversed(production))
+        else:
+          return False
+      else:
+        return False
+  
+    return True
+
 
 # Test cases of 1, 2 and 3
 test_cases = ["(a+a)*a$", "a*(a/a)$", "a(a+a)$"]
@@ -25,4 +44,4 @@ for test_case in test_cases:
     if parse(test_case):
       print("String is accepted or valid.")
     else:
-      print("strig is not accepted or valid.")
+      print("String is not accepted or invalid.")
